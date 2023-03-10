069a54062532f11b25c091d9c1f078d0d0052a18
Lcuky12 <118757873+Lcuky12@users.noreply.github.com>
2023-03-10 21:16:10 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp46
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName;
+            int characterCount = 1;
+            char userInputySbool;
+            string numberLines;
+            string characters = "";
+
+            Console.Write(" Введите имя ");
+            userName = Console.ReadLine();
+            Console.Write(" Введите символ ");
+            userInputySbool = Convert.ToChar(Console.Read());
+
+            numberLines = userInputySbool + userName + userInputySbool;
+
+            for (int i = 0; i <= userName.Length + characterCount; i++)
+            {
+                characters += userInputySbool;
+            }
+
+            Console.WriteLine(characters);
+            Console.WriteLine(numberLines);
+            Console.WriteLine(characters);
+        }    
+    }
+}
